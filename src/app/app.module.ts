35e5e7a4e28eb559e789fd860c1728dc75ba7f77
ian otieno <ianotieno23@gmail.com>
2023-09-18 12:08:23 +0300

    css update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyFirstCompComponent } from './my-first-comp/my-first-comp.component';
+import { MessageDetailsComponent } from './message-details/message-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyFirstCompComponent
+    MyFirstCompComponent,
+    MessageDetailsComponent
   ],
   imports: [
     BrowserModule,
