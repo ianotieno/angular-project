645eb595812b39a8bb504ab6b1bf704807166560
ian otieno <ianotieno23@gmail.com>
2023-09-18 22:54:35 +0300

    update

--- src/app/services/my-first.service.ts
+++ src/app/services/my-first.service.ts
@@ -4,6 +4,34 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MyFirstService {
-
-  constructor() { }
+ messages:Array<any>=[]
+ init(){
+  this.insert({
+    name: 'ian',
+    email:'ianotieno@',
+    message:'hello'
+  });
+  this.insert({
+    name: 'john',
+    email:'ianotieno@',
+    message:'hello-john'
+  });
+  this.insert({
+    name: 'paul',
+    email:'ianotieno@',
+    message:'hello-paul'
+  });
+ }
+  constructor() {
+    this.init
+   }
+  insert(message:{name:string,email:string,message:string}){
+    this.messages.push(message)
+  }
+  getAllMessages(){
+    return this.messages
+  }
+  deleteMessage(index:number){
+    this.messages.slice(index,1)
+  }
 }
