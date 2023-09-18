15b94f2177a6a9471f0cc64d06f5a4580ed412a8
ian otieno <ianotieno23@gmail.com>
2023-09-18 09:43:49 +0300

    update

---
+++ src/app/my-first-comp/my-first-comp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-first-comp',
+  templateUrl: './my-first-comp.component.html',
+  styleUrls: ['./my-first-comp.component.css']
+})
+export class MyFirstCompComponent {
+name:string =""
+email:string=""
+message:string=""
+}
